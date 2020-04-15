8a6e873a5363ac2cac401eae1e6155e0fc9e7b9e
Siim Tiilen <eritikass@gmail.com>
2020-04-15 13:47:42 +0300

    add calculate

--- handler.js
+++ handler.js
@@ -1,12 +1,22 @@
 'use strict';
 
 module.exports.hello = async event => {
+  let { queryStringParameters } = event;
+  if (!queryStringParameters) {
+    queryStringParameters = {};
+  }
+  const a = parseInt(queryStringParameters.a, 10) || 0;
+  const b = parseInt(queryStringParameters.b, 10) || 0;
+
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+        a,
+        b,
+        sum: a + b,
+        // message: 'Go Serverless v1.0! Your function executed successfully!',
+        // input: event,
       },
       null,
       2
